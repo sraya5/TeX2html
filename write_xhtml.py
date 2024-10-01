0be7fc64772be34a414cd7e55087a1e69f19f7bb
sraya5 <sraya.ansbacher@mail.huji.ac.il>
2024-10-01 18:37:26 +0300

    commit

--- write_xhtml.py
+++ write_xhtml.py
@@ -1,9 +1,35 @@
-from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import Element, ElementTree
 from json import load
+from sys import argv
+from read_LaTeX import parse_document
 from document import *
 
-with open('name2tag.json', 'r', encoding='utf8') as f:
+with open('name2tag.json', 'r', encoding='utf8') as file:
     NAME2TAG = load(f)
+
+
+def one_node(node):
+    if type(node) is Environment:
+        element = Element(NAME2TAG[node.environment_name]['tag'], {'class': node.environment_name}, text='', tail='')
+    elif type(node) is Section:
+        element = Element('section', text='', tail='')
+    else:
+        raise TypeError('invalid node')
+    return element
+
+
+def scan(node):
+    element = one_node(node)
+    last = None
+    for child in node.children:
+        if type(child) in (Environment, Section):
+            last = scan(child)
+        elif type(child) is FreeText:
+            if last:
+                last.tail += FreeText.content
+            else:
+                element.text += FreeText.content
+    return element
 
 
 def create_head(title='', css_path=''):
@@ -21,18 +47,33 @@ def create_head(title='', css_path=''):
     return head
 
 
-def one_node(node):
-    if type(node) is Environment:
-        element = Element(NAME2TAG[node.environment_name]['tag'], {'class': node.environment_name})
-    elif type(node) is Section:
-        element = Element('section')
-    else:
-        raise TypeError('invalid node')
-    return element
-
+def create_body(doc: Document):
+    body = Element('body')
+    for child in doc.children:
+        child = scan(child)
+        body.append(child)
+    return body
 
 
 def convert(doc: Document, css_path=''):
     root = Element('html', {'xmlns': 'http://www.w3.org/1999/xhtml'})
     head = create_head(doc.title.title, css_path)
-    body = Element('body')
+    body = create_body(doc)
+    root.extend((head, body))
+    tree = ElementTree(root)
+    return tree
+
+
+def main():
+    if len(argv) != 3:
+        raise Exception
+
+    with open(argv[1], 'r', encoding='utf8') as file:
+        content = file.read()
+        doc = parse_document(content)[0]
+        tree = convert(doc)
+        tree.write(argv[2])
+
+
+if __name__ == '__main__':
+    main()
